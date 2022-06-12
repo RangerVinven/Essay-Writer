b6fba02cd40d175e3f1f1558b8ec09655db6e4fb
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-12 23:43:05 +0100

    Moved the state variables back

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,10 @@ import Draft from './pagesAndComponents/pages/draft/Draft';
 import Outline from './pagesAndComponents/pages/outline/Outline';
 
 function App() {
-
-	const [outlines, setOutlines] = useState<Object[]>([
-        {
-            id: 0,
-            title: "Hello",
-            drafts: [""]
-        },
-        {
-            id: 1,
-            title: "Test",
-            drafts: [""]
-        }
-    ]);
-    const [currentOutline, setCurrentOutline] = useState(""); // For the outline the user is currently entering
-
-    const addOutline = () => {
-        setOutlines([...outlines, {
-			id: outlines.length-1,
-			title: currentOutline,
-			drafts: [""]
-		}]);
-        setCurrentOutline("");
-    }
-
 	return (
 		<Router>
 			<Routes>
-				<Route path="/outline" element={<Outline outlines={outlines} addOutline={addOutline} currentOutline={currentOutline} setCurrentOutline={setCurrentOutline} />} />
+				<Route path="/outline" element={<Outline />} />
 				<Route path="/draft" element={<Draft />} />
 				<Route path="/" element={<Draft />} />
 			</Routes>
--- src/pagesAndComponents/pages/outline/Outline.tsx
+++ src/pagesAndComponents/pages/outline/Outline.tsx
@@ -1,33 +1,71 @@
 import React, { useState } from 'react';
 
-interface Props {
-    addOutline: Function,
-    outlines: Object[],
-    
-    currentOutline: string,
-    setCurrentOutline: Function
-}
+export default function Outline() {
 
-export default function Outline(props: Props) {
+    const [outlines, setOutlines] = useState<Object[]>([
+        {
+            id: 0,
+            title: "Hello",
+            drafts: [""]
+        },
+        {
+            id: 1,
+            title: "Test",
+            drafts: [""]
+        }
+    ]);
+    const [currentOutline, setCurrentOutline] = useState(""); // For the outline the user is currently entering
 
-    // const [outlines, setOutlines] = useState<string[]>([]);
-    // const [currentOutline, setCurrentOutline] = useState(""); // For the outline the user is currently entering
+    const addOutline = () => {
+        setOutlines([...outlines, {
+			id: outlines.length,
+			title: currentOutline,
+			drafts: [""]
+		}]);
+        
+        console.log(outlines.length);
+        
+        setCurrentOutline("");		
+    }
+
+
+    const deleteOutline = (outlineID: number) => {
+		let newOutlines: any = outlines;
+		
+		// Deletes the outline from the newOutlines array        
+		newOutlines.splice(outlineID, 1);
+
+		// Loops through newOutlines and changes the id of each outline to the correct one (the one where it matches the position in the array)
+		for(let i = 0; i < newOutlines.length; i++) {
+			newOutlines[i].id = i;
+		}
+        
+		setOutlines(newOutlines);
+	}
 
     return (
-        <div>
-            <div>
-                <input type="text" className="border-2 border-slate-200 rounded-md mr-3" placeholder="Draft" value={props.currentOutline} onChange={e => {props.setCurrentOutline(e.target.value)}}/>
-                <button className="bg-green-500 px-3 text-white font-bold rounded-md" onClick={() => props.addOutline()}>Add</button>
-            </div>
+        <div className="flex items-center justify-center bg-slate-200 h-screen w-full">
+            <div className="w-2/5 h-5/6 my-10 bg-white rounded-md p-5">
+                <div className="w-full h-full">
+                    <div className="flex justify-center">
+                        <input type="text" className="border-2 border-slate-200 rounded-md pl-1 mr-3 w-3/5" placeholder="Draft" value={currentOutline} onChange={e => {setCurrentOutline(e.target.value)}}/>
+                        <button className="bg-green-500 px-3 text-white font-bold rounded-md" onClick={() => addOutline()}>Add</button>
+                    </div>
 
-            <div className="mt-10">
-                <ul>
-                    {
-                        props.outlines.map((outline: any) => {
-                            return <li>{outline.title}</li>
-                        })
-                    }
-                </ul>
+                    <div className="mt-10">
+                        <ul>
+                            {
+                                outlines.map((outline: any) => {
+                                    return <li key={outline.title} className="mb-4">
+                                            <h1 className="text-2xl inline mr-3">{outline.title}</h1>
+                                            <button type="submit" onClick={() => deleteOutline(outline.id)} className="bg-red-500 font-bold p-1 rounded-md">Delete</button>
+                                        </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    
+                </div>
             </div>
         </div>
         
